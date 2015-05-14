96c55f9b4644f22c6d9718f9dd1367d1283093df
Ali Hamidi <ahamidi@me.com>
2015-05-14 11:16:27 -0700

    add examples

--- mapreduce_test.go
+++ mapreduce_test.go
@@ -1,6 +1,7 @@
 package mapreduce
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -39,4 +40,57 @@ func TestMapReduce(t *testing.T) {
 	if result != 4950 {
 		t.Fail()
 	}
+}
+
+type ExampleMapReducer struct{}
+
+func (mr *ExampleMapReducer) Map(in chan interface{}, out chan interface{}) {
+	for v := range in {
+		out <- v
+	}
+}
+
+// Example Map Function
+func (mr *ExampleMapReducer) Example_map(in chan interface{}, out chan interface{}) {
+	for v := range in {
+		out <- v
+	}
+}
+
+func (mr *ExampleMapReducer) Reduce(in chan interface{}) interface{} {
+	res := 0
+	for v := range in {
+		res += v.(int)
+	}
+	return res
+}
+
+// Example Reduce Function
+func (mr *ExampleMapReducer) Example_reduce(in chan interface{}) interface{} {
+	res := 0
+	for v := range in {
+		res += v.(int)
+	}
+	return res
+}
+
+func Example() {
+	conf := NewMapReduceConfig()
+
+	// Feed input channel
+	go func(in chan interface{}) {
+		for i := 0; i < 100; i++ {
+			in <- i
+		}
+		close(in)
+	}(conf.InChan)
+
+	var mr ExampleMapReducer
+
+	result, _ := Run(&mr, conf)
+
+	fmt.Println(result)
+	// Output:
+	// 4950
+
 }
